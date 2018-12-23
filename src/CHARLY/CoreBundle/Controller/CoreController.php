fd12c29ba4811800f8b1ea87f328b6054526e641
charles <4charles2@users.noreply.github.com>
2018-12-24 00:52:17 +0100

    test for xdebug is good for setting see in php.ini php -v php7.2.4

--- src/CHARLY/CoreBundle/Controller/CoreController.php
+++ src/CHARLY/CoreBundle/Controller/CoreController.php
@@ -9,17 +9,14 @@ class CoreController extends Controller
 {
     public function indexAction(){
         /*
-         *
          $rss = null;
         $i = 0;
         foreach( NewsController::flux as $key => $url)
             $rss[$i++] = $url->getFlux() ? : $rss[$i] = "Le Flux Rss : ".$url." n'à pas pu être chargé ";
 */
-phpinfo();
         return $this->render('CHARLYCoreBundle:Core:index.html.twig', array('flux' => $this->get('charly_news.fluxRss')->getFlux() ));
     }
     public function contactAction(){
-        $testXdebug = O;
         $this->addFlash('info', 'La page de contact n\'est pas encore disponible. Merci de revenir plus tard');
         return $this->redirectToRoute('charly_core_homepage');
     }
