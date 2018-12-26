5ec734f900ecd074e887b7330c770260ffa4b6a9
charles <4charles2@users.noreply.github.com>
2018-12-26 09:34:25 +0100

    Create service ApplicationMailer in PlatformBundle

---
+++ src/CHARLY/PlatformBundle/Email/ApplicationMailer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2018. Toute reproduction ou utilisation est interdite sans l'accord de l'auteur
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: charly
+ * Date: 26/12/2018
+ * Time: 09:32
+ */
+
+namespace CHARLY\PlatformBundle\Email;
+
+use CHARLY\PlatformBundle\Entity\application
+
+
+class ApplicationMailer
+{
+
+}
