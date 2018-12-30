df4ef1d1740825c5a56f2083d89c824f99958128
charles <4charles2@users.noreply.github.com>
2018-12-30 16:45:23 +0100

    layout body html send mail in ApplicationMailer

--- src/CHARLY/PlatformBundle/Email/ApplicationMailer.php
+++ src/CHARLY/PlatformBundle/Email/ApplicationMailer.php
@@ -36,22 +36,31 @@ class ApplicationMailer
      */
     public function sendNewNotification(Application $application){
         if($application->getAdvert()->getEmail() != NULL ) {
-            $message = new \Swift_Message(
-                'Nouvelle candidature',
-                'Vous avez reçu une nouvelle candidature de '
-                .$application->getAuthor()
-                .$application->getEmail()
-                .' Voici sa réponse : '
-                .$application->getContent()
-                .' Cette réponse concerne votre annonce : '
-                .$application->getAdvert()->getTitle()
-                .' '.$application->getContent()
-                .' Mise en ligne le '.$application->getDate()->getOffset()
-            );
-            //correction du
-            $message
+            $message = (new \Swift_Message('Nouvelle candidature'))
                 ->addTo($application->getAdvert()->getEmail())
-                ->addFrom('admin@charles-tognol.fr');
+                ->addFrom('admin@charles-tognol.fr')
+                ->setBody(
+                    '<div>
+                        <h1>Vous avez reçu une nouvelle candidature</h1>'
+                        .'<ul>
+                            <li> de : '.$application->getAuthor().'</li>
+                            <li>Email : '.$application->getEmail().'</li>
+                        </ul>
+                        <h3> Voici sa réponse : </h3>
+                        <div style="border: 1px solid black; padding: 10px; text-align: centers;">
+                            <p>'.$application->getContent().'</p>
+                        </div>
+                        <h3> Cette réponse concerne votre annonce : </h3>
+                        <div style="padding: 10px; border: 1px solid black;">
+                            <p>'.$application->getAdvert()->getTitle().'</p>
+                            <p> '.$application->getAdvert()->getContent().'</p>
+                        </div>
+                        <cite>Mise en ligne le '.$application->getDate()->format('d-m-y \à h:i:s').'</cite>
+                    </div>',
+                    'text/html')
+                ->setReadReceiptTo('charly.learn@gmail.com');
+
+
             $this->mailer->send($message);
         }
     }
